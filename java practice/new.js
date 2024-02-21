32e430aeaa7e0a9cbba34f91f1fd01723e07af01
Peo2 <ajeigbefelix033@gmail.com>
2024-02-21 12:53:36 +0100

    netflixhome.html

---
+++ java practice/new.js
@@ -0,0 +1,35 @@
+//var Name = 'felix';
+//var age = 33;
+//var messag = 'my name is ' + Name + 'I am ' + age+ 'years old'
+
+//var amountInAccount = 3000;
+//var sneakers = 250;
+//var socks = 2500;
+//var wine = 250;
+//var accountBalance = amountInAccount - sneakers - socks - wine;
+//if (accountBalance < amountInAccount - sneakers - socks - wine) {
+  //  console.log('you have a balance of ' + accountBalance);
+//}
+//else if(accountBalance >  amountInAccount - sneakers - socks - wine ){
+  //  console.log('your account balance is too low, kindly fund your account');
+//}
+//else{
+  //  console.log('your account balance is' + accountBalance + ' It is too low for you to purchase this item ')
+//}
+//var cat1 = 4;
+//var cat2 = 11;
+//var cat3 = 5;
+//if (cat1 > cat2 && cat1 > cat2){
+  //  console.log('cat 1 is the cuttest');
+//} else if (cat2 > cat1 && cat2 > cat3){
+  //  console.log('cat2 is the cuttest');
+//} else if(cat3 > cat1 && cat3 > cat2);{
+  //  console.log('cat 3 is the cuttest')
+//}
+var studentNames = ['segun','ade','laide','eze'];
+var pupilsList = [];
+pupilsList.push(studentNames[2]);
+console.log(pupilsList);
+var index = pupilsList.indexOf('segun');
+console.log(index);
+
